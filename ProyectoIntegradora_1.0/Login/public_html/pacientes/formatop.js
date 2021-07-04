b4ce38856734e702e458f8cdcab71d8c4f409d06
EduardoEscorza <86625811+EduardoEscorza@users.noreply.github.com>
2021-07-04 14:01:17 -0500

    Add files via upload

---
+++ ProyectoIntegradora_1.0/Login/public_html/pacientes/formatop.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function btn_consulta(){
+   formulario_registre.style.display = "none";
+   contenedor_login_registre.style.left = "10px";
+   formulario_login.style.display = "block";
+   caja_trasera_register.style.opacity = "1";
+   caja_trasera_login.style.opacity = "0";  
+}
+
